133e3d25cf66a18e0fd7b8e1ecd97e9091bb5f2c
LamsyA <olamide.adetula.1744@gmail.com>
2023-06-16 06:14:03 +0100

    Smart contract

--- contracts/EasyNFT.sol
+++ contracts/EasyNFT.sol
@@ -4,15 +4,19 @@ pragma solidity ^0.8.12;
 import "erc721a/contracts/ERC721A.sol";
 
 contract EasyNFT is ERC721A{
+
+    uint256 Public MAX_SUPPLY_QUANTITY = 5;
     constructor() ERC721A("EasyNFT", "EST") {}
 
     function mint(uint256 quantity) external payable {
         // quantity is the amount nfts you want to mint
-        _mint(msg.sender, quantity);
+        require(totalSupply() + quantity <= MAX_SUPPY_QUANTITY,"maximum quantity ");
+        _safeMint(msg.sender, quantity);
     }
 
-    
-
+    function _baseMint() internal {view override returns (string memory){
+        returns https://gateway.pinata.cloud/ipfs/QmbhzuWuyYicC9Qj7xS2Q49QY2tkhiBXydYV3u1NtTgHet/;
+    }
     function promptDescription() external pure returns (string memory) {
         return
             "Hunter lost in African forest finds mystical village, bridges two communities through wisdom and unity.";
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+ 
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const EasyNFT = await hre.ethers.getContractFactory("EasyNFT");
+  const easynft = await EasyNFT.deploy("EasyNFT", "EST");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await easynft.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    ` deployed to: ${easynft.address}`
   );
 }
 
